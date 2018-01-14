604dd2fe2544a8b2279d584833031c0557b7997d
Andreya <avalabek@yahoo.com>
2018-01-14 12:07:08 -0700

    tweets, movie, spotify function without defaults

--- liri.js
+++ liri.js
@@ -65,7 +65,7 @@ function music() {
             console.log('Error occurred: ' + err);
             return;
         } else {
-            //tried searching for release year! Spotify doesn't return this!
+            
             console.log("Artist: " + data.tracks.items[0].artists[0].name);
             console.log("Song: " + data.tracks.items[0].name);
             console.log("Album: " + data.tracks.items[0].album.name);
